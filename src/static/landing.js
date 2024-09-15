2f96e20bcff7964c7d9b007432635165cf098171
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-09-15 06:07:29 -0600

    Main page

---
+++ src/static/landing.js
@@ -0,0 +1,24 @@
+// Mobile Menu Toggle
+document.addEventListener('alpine:init', () => {
+    Alpine.data('mobileMenu', () => ({
+        openMenu: false,
+
+        toggleMenu() {
+            this.openMenu = !this.openMenu;
+        },
+
+        closeMenu() {
+            this.openMenu = false;
+        }
+    }));
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
